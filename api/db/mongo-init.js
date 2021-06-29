81a7a450063ea3de3f8c99f75d7407001338e287
brteo <51654355+brteo@users.noreply.github.com>
2021-06-29 22:35:12 +0200

    mongo-init sh instead js

--- api/db/mongo-init.js
+++
@@ -1,9 +0,0 @@
-// eslint-disable-next-line no-undef
-db.createUser({
-	user: 'user',
-	pwd: 'user0201',
-	roles: [
-		{ role: 'dbOwner', db: 'tradingvg' },
-		{ role: 'readWrite', db: 'tradingvg' }
-	]
-});
