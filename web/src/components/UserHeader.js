0c7d40e48e4ceeb4af933b3d38c8b3f8b444299d
MEBoo <alan@meblabs.com>
2021-08-02 14:58:21 +0200

    fix userheader

--- web/src/components/UserHeader.js
+++ web/src/components/UserHeader.js
@@ -6,10 +6,7 @@ const UserHeader = props => {
 
 	// alt="User profile header image"
 	return (
-		<div
-			className="userHeader"
-			style={user && user.header ? { backgroundImage: 'url(' + process.env.PUBLIC_URL + user._id + ')' } : {}}
-		>
+		<div className="userHeader" style={user && user.header ? { backgroundImage: 'url(' + user.header + ')' } : {}}>
 			{props.children}
 		</div>
 	);
