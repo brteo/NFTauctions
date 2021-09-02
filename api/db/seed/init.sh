82982df00124743782c6e04cd7f423f465e20333
brteo <matteo@meblabs.com>
2021-09-02 18:33:24 +0200

    fix db seed

--- api/db/seed/init.sh
+++ api/db/seed/init.sh
@@ -6,6 +6,6 @@ cd seed
 
 dir=$(ls *.json)
 for file in $dir; do
-	mongo -u $MONGO_DATABASE_USERNAME -p $MONGO_DATABASE_PASSWORD $MONGO_INITDB_DATABASE --eval 'db.'${file%.*}'.drop()'
+	mongo -u $MONGO_DATABASE_USERNAME -p $MONGO_DATABASE_PASSWORD $MONGO_INITDB_DATABASE --eval 'db.'${file%.*}'.remove({})'
 	mongoimport -u $MONGO_DATABASE_USERNAME -p $MONGO_DATABASE_PASSWORD --collection "${file%.*}" --db $MONGO_INITDB_DATABASE /seed/$file --jsonArray
 done
